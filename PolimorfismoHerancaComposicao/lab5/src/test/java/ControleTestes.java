6501aeedcef4e4ea6c593cd585e341b87be0bbb4
GustavoSouza07 <gustavosouzaloure@gmail.com>
2025-05-04 22:37:01 -0300

    arquivo de dependencias java

--- PolimorfismoHerancaComposicao/lab5/src/test/java/ControleTestes.java
+++ PolimorfismoHerancaComposicao/lab5/src/test/java/ControleTestes.java
@@ -1,3 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.Classes.Controle;
+import com.Classes.Funcionario;
+import com.Excecoes.FuncionarioExistente;
+
 public class ControleTestes {
     
+    private Controle controle;
+
+    @BeforeEach
+    void setUp() {
+        controle = new Controle();
+    }
+    
+    @Test
+    void testAdicionarFuncionarioComSucesso() throws FuncionarioExistente {
+        Funcionario funcionario = controle.adicionarFuncionario("João");
+        assertNotNull(funcionario);
+        assertEquals("João", funcionario.getNome());
+    }
+
+    @Test
+    void testAdicionarFuncionarioExistente() throws FuncionarioExistente {
+        controle.adicionarFuncionario("Maria");
+        assertThrows(FuncionarioExistente.class, () -> {
+            controle.adicionarFuncionario("Maria");
+        });
+    }
+
+
 }
